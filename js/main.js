0b6fac80a8d906d86e2f1543ab23accb5337eb0c
sandra <sandrarocio12oc@gmail.com>
2021-08-02 15:49:25 -0500

    add sections

--- js/main.js
+++ js/main.js
@@ -307,5 +307,20 @@ jQuery(function ($) {
       });
    }
 
-   
+   $('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:3
+        }
+    }
+})
 });
